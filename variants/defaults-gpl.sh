91f0989a9c72e01d4ec706612778e047a3debaee
Sebastian Krajewski <mail@wieku.me>
2022-08-25 01:06:36 +0200

    Add danser defaults

--- variants/defaults-gpl.sh
+++ variants/defaults-gpl.sh
@@ -1,4 +1,26 @@
-FF_CONFIGURE="--enable-gpl --enable-version3 --disable-debug"
+FF_CONFIGURE="--enable-gpl --enable-version3 --disable-debug
+--disable-autodetect
+
+--disable-ffplay
+--disable-avdevice
+--disable-postproc
+
+--enable-nvenc
+
+--disable-decoders
+--enable-decoder=h263,h264,hevc,mjpeg,vp9,rawvideo,pcm_f32le,pcm_f32be
+
+--disable-encoders
+--enable-encoder=libx264,libx265,h264_nvenc,hevc_nvenc,h264_qsv,hevc_qsv,aac,libmp3lame,libopus,flac,rawvideo
+
+--disable-demuxers
+--enable-demuxer=matroska,mov,avi,flv,rawvideo,wav,pcm_f32le,pcm_f32be
+
+--disable-muxers
+--enable-muxer=mov,rawvideo,mp4,matroska,mp3,flac,ogg
+
+--disable-protocols
+--enable-protocol=file,pipe"
 FF_CFLAGS=""
 FF_CXXFLAGS=""
 FF_LDFLAGS=""
