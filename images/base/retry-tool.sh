75c11af7de8e3942817391a1959d88fb821665a9
Sebastian Krajewski <mail@wieku.me>
2022-08-25 03:56:25 +0200

    Raise timeout to 5 minutes

--- images/base/retry-tool.sh
+++ images/base/retry-tool.sh
@@ -4,7 +4,7 @@ set -xe
 RETRY_COUNTER=0
 MAX_RETRY=15
 while [[ $RETRY_COUNTER -lt $MAX_RETRY ]]; do
-    timeout 120 "$@" && break || sleep 10
+    timeout 300 "$@" && break || sleep 10
     RETRY_COUNTER=$(( $RETRY_COUNTER + 1 ))
     echo "Retry $RETRY_COUNTER..."
 done
