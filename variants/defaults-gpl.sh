87e790dd50d1786e0ec85688636ac39d99a1de1e
Sebastian Krajewski <mail@wieku.me>
2024-02-26 13:32:44 +0100

    Rollback lavfi stuff

--- variants/defaults-gpl.sh
+++ variants/defaults-gpl.sh
@@ -9,13 +9,13 @@ FF_CONFIGURE="--enable-gpl --enable-version3 --disable-debug
 --enable-amf
 
 --disable-decoders
---enable-decoder=h263,flv,h264,hevc,mjpeg,vp9,rawvideo,pcm_f32le,pcm_f32be,wrapped_avframe
+--enable-decoder=h263,flv,h264,hevc,mjpeg,vp9,rawvideo,pcm_f32le,pcm_f32be
 
 --disable-encoders
 --enable-encoder=libx264,libx265,h264_nvenc,hevc_nvenc,av1_nvenc,h264_qsv,hevc_qsv,h264_amf,hevc_amf,aac,libmp3lame,libopus,flac,rawvideo
 
 --disable-demuxers
---enable-demuxer=matroska,mov,avi,flv,rawvideo,wav,pcm_f32le,pcm_f32be,lavfi
+--enable-demuxer=matroska,mov,avi,flv,rawvideo,wav,pcm_f32le,pcm_f32be
 
 --disable-muxers
 --enable-muxer=mov,rawvideo,mp4,matroska,mp3,flac,ogg,null
