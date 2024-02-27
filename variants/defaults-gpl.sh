e58bb02741a46f03e419af8440db4bc9fd698fe6
Sebastian Krajewski <mail@wieku.me>
2024-02-27 01:49:20 +0100

    Nvm

--- variants/defaults-gpl.sh
+++ variants/defaults-gpl.sh
@@ -11,7 +11,7 @@ FF_CONFIGURE="--enable-gpl --enable-version3 --disable-debug
 --enable-decoder=h263,flv,h264,hevc,mjpeg,vp9,rawvideo,pcm_f32le,pcm_f32be
 
 --disable-devices
---enable-device=lavfi
+--enable-indev=lavfi
 
 --disable-encoders
 --enable-encoder=libx264,libx265,h264_nvenc,hevc_nvenc,av1_nvenc,h264_qsv,hevc_qsv,h264_amf,hevc_amf,aac,libmp3lame,libopus,flac,rawvideo,wrapped_avframe
